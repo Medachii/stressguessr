e60c5ff93639ee2fd96276ee57e3d80550e22d2c
N2L <noe-laurent.laurent@telecomnancy.eu>
2023-01-23 09:42:44 +0100

    Fin du prétraitement

--- data/pre.py
+++ data/pre.py
@@ -6,20 +6,21 @@ import requests
 import time
 import json
 
-isAt='Accident'        #TODO : change the value of isAt to the word you want to start from
+isAt='aircraft'        #TODO : change the value of isAt to the word you want to start from
 flag = 0
 # open the file dictionnary.csv in the data folder
-with open('dictionary.csv', 'r') as f:
+with open('oxford_3000.txt', 'r') as f:
     # create a reader object
     reader = csv.reader(f)
-    f=open("data.txt","a")
+    output=open("data.txt","a")
     for line in reader:
+        
         print(line[0])
         if line[0] == isAt:
             flag = 1
         if flag==0:
             continue 
-
+        time.sleep(0.3)
         #get the json file from https://api.dictionaryapi.dev/api/v2/entries/en/{word} 
         r = requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{line[0].lower()}')
         
@@ -33,10 +34,10 @@ with open('dictionary.csv', 'r') as f:
             #if the word is not found in the dictionary, continue
             if not 'title' in r and r[0]['word'] == line[0].lower():
                 #if the audio exists 
-                if r[0]['phonetics'] != [] and r[0]['phonetics'][0]['audio'] != '':
+                if 'phonetic' in r[0] and r[0]['phonetic'] != None:
                     #write the word, the phonetic and the audio in the file data.txt
                     print("YOUPI")
-                    f.write(f"{line[0]}\n")
+                    output.write(f"{line[0]}\n")
                 
 
                 
