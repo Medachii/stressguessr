74f5ed8aec8fc18b5791572b1ce82b2bf2f22162
N2L <noe-laurent.laurent@telecomnancy.eu>
2023-02-01 17:12:14 +0100

    Affichage du mot fonctionnel

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,16 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import raw from './data.txt'
+
+
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      choosenWord: "",
+    };
+  }
+
+  componentDidMount() {
+    this.wordChoose();
+  }
+
+  wordChoose = async () => {
+    const response = await fetch(raw);
+    const text = await response.text();
+    const wordList = text.split("\n");
+    const word = wordList[Math.floor(Math.random() * wordList.length)];
+    console.log("Choosen Word : " + word);
+    this.setState({ choosenWord: word });
+  };
+
   render() {
-    
-    wordChoose();
-    
     return (
       <div className="game">
         <h1>stressguessr</h1>
-        <Word word="hello" />         
+        <Word choosenWord={this.state.choosenWord} />
       </div>
     );
   }
@@ -31,9 +51,9 @@ class Word extends React.Component {
     return (
       <div className="word">
         <h2>
-          
-          {this.props.word.split('').map((letter) => <Letter letter={letter} />)}
-
+          {this.props.choosenWord.split("").map((letter) => (
+            <Letter letter={letter} />
+          ))}
         </h2>
       </div>
     );
@@ -44,14 +64,6 @@ function letterPushed() {
   console.log("letter pushed");
 }
 
-function wordChoose(){
-  var fr = new FileReader();
-  let file = open("words.txt", "r");
-
-  let wordChoosen = fr.readAsText(file);
-  console.log(wordChoosen);
-
-}
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
