f3d2582d54be84f598e659bf9b7c8716f63c35db
N2L <noe-laurent.laurent@telecomnancy.eu>
2023-03-07 17:36:09 +0100

    jeu fonctionnel v1

--- my-app/src/components/Game.js
+++ my-app/src/components/Game.js
@@ -2,10 +2,16 @@ import React, { useEffect, useState } from "react";
 import "../index.css";
 import raw from "../data.txt";
 import Word from "./Word";
+import styled from "styled-components";
 
 const Game = () => {
 
   const [chosenWord, setChosenWord] = useState("");
+  const [gamePoints, setGamePoints] = useState(0);
+  const [flag, setFlag] = useState(1);
+  const [round, setRound] = useState(1);
+
+
   //TODO
 
   const wordChoose = async () => {
@@ -17,9 +23,49 @@ const Game = () => {
     setChosenWord((chosenWord) => word);
   };
 
+
+
+
+
+
+  function addPoints(points) {
+    if (flag == 1) {
+      setGamePoints((gamePoints) => gamePoints + points);
+      console.log("gamePoints : " + gamePoints);
+      console.log("===============================================================================================");
+      document.getElementById("#next").style.display = "block";
+
+      setFlag((flag) => 0);
+    }
+
+
+  }
+
+  function newgame() {
+    setGamePoints((gamePoints) => 0);
+    wordChoose();
+    document.getElementById("#next").style.display = "none";
+    setFlag((flag) => 1);
+    setRound((round) => 1);
+  }
+
+  function next() {
+    wordChoose();
+    document.getElementById("#next").style.display = "none";
+    setFlag((flag) => 1);
+    setRound((round) => round + 1);
+    if (round == 10) {
+      alert("You got " + gamePoints + " points !");
+      newgame();
+      setRound((round) => 1);
+    }
+
+  }
+
   //Equivalent à componentDidMount
   useEffect(() => {
     //TODO
+
     wordChoose();
   }, []);
 
@@ -29,9 +75,14 @@ const Game = () => {
   return (
     <div className="game">
       <h1>stressguessr</h1>
-      <Word chosenWord={chosenWord} />
+      <h2>Points : {gamePoints}</h2>
+      <h2>Round : {round}/10</h2>
+      <Word chosenWord={chosenWord} updatePoints={addPoints} />
+      <button onClick={newgame}>New Game</button>
+      <button id="#next" onClick={next} style={{ display: "none" }}>Next</button>
     </div>
   );
 };
 
 export default Game;
+
--- my-app/src/components/Word.js
+++ my-app/src/components/Word.js
@@ -1,13 +1,189 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Letter from "./Letter";
 
-const Word = ({chosenWord}) => {
+const Word = ({ chosenWord, updatePoints }) => {
+
+  const [finalStress,setFinalStress] = useState("");
+
+
+  const phonemedictionnary = {
+    '/': ['none'],
+    'h': ['h'],
+    'ə': ['e', 'a', 'ure', 'er', 'io', 'o', 're', 'ou', ''],
+    'o': ['o'],
+    'ʊ': ['u', 'oul', 'o', 'ugh'],
+    '.': [''],
+    'ɪ': ['i', 'a', 'e', 'u'],
+    'l': ['l', 'le', 'll'],
+    'i': ['i', 'ee', 'ea', 'y', 'e'],
+    't': ['t', 'tt', 'te',''],
+    'ˈ': [''],
+    'ˌ': [''],
+    'b': ['b'],
+    'ɑ': ['a'],
+    'a': ['a', 'o'],
+    'æ': ['a'],
+    'e': ['e', 'a'],
+    'ɝ': ['ear', 'ur'],
+    'ɒ': ['o'],
+    'ʌ': ['u'],
+    'ɔ:': ['o', 'a', 'ou'],
+    'u': ['u', 'oo', 'ue', 'ough'],
+    'aɪ': ['ai', 'i', 'eye'],
+    'aʊ': ['ow', 'ou'],
+    'eɪ': ['ay', 'ey', 'ei'],
+    'oʊ': ['o', 'ou', 'ow'],
+    'ɔɪ': ['oi', 'oy', 'oye'],
+    'ɛ': ['e', 'ea'],
+    'ɜ': ['o', 'or'],
+    'ɹ': ['r', 're', 'rar'],
+    'd': ['d'],
+    'f': ['f', 'ph','ff'],
+    'g': ['g'],
+    'j': ['y', 'j', ''],
+    'k': ['k', 'c', 'ck', 'x', 'ch', 'q'],
+    'm': ['m'],
+    'n': ['n'],
+    'ŋ': ['ng', 'n'],
+    'p': ['p', 'pp'],
+    'r': ['r', 'rr'],
+    's': ['s', 'ss','ce', ''],
+    'ʃ': ['sh', 'ch', 'ss', ''],
+    'tʃ': ['ch', 'tch'],
+    'θ': ['th'],
+    'ð': ['th'],
+    'v': ['v'],
+    'w': ['w', 'u'],
+    'z': ['z', 'x', 's', 'si'],
+    'ʒ': ['zh', 'j', 's',],
+    'd͡ʒ': ['j', 'g', 'ge', 'dge'],
+    'ː': [''],
+    'ɡ': ['g', 'gg', ''],
+    '(': [''],
+    ')': [''],
+    'ɔ': ['o', 'a', 'ou'],
+    'ɚ': ['ar', 'er'],
+    'ɘ': ['e', 'a','o'],
+  }
+  function extractPhoneme(data) {
+    var phoneme = data[0].phonetics[0].text;
+    //tant que le phoneme est null
+    let i = 1;
+    while (phoneme == null) {
+      phoneme = data[0].phonetics[i].text;
+      i++;
+    }
+    return phoneme;
+
+  }
+  function getPhoneme(word, callback) {
+    const Http = new XMLHttpRequest();
+    const url = "https://api.dictionaryapi.dev/api/v2/entries/en/{}".replace('{}', word);
+    Http.open("GET", url);
+    Http.send();
+    Http.responseType = "json";
+    Http.onload = () => {
+      if (Http.readyState === 4 && Http.status === 200) {
+        const data = Http.response;
+        let extractedPhoneme = extractPhoneme(data);
+        console.log("extracted : " + extractedPhoneme);
+        callback(extractedPhoneme);
+      }
+    };
+  }
+  let stress = -2;
+  let wordFinished = false;
+
+  function findtheStress(word) {
+    console.log("findtheStress");
+    console.log("word : " + word);
+
+    const phoneme = getPhoneme(word, (phoneme) => {
+      console.log("pho " + phoneme);
+      let index = 1;
+      let finalWord = "";
+
+      function findRecursiveStress(phoneme, index, wordToFind, finalWord) {
+
+        if (wordFinished === true) {
+          return;
+        }
+
+        console.log("----------------------------------------------------");
+        console.log("findRecursiveStress");
+        console.log("phoneme[index] : " + phoneme[index]);
+        console.log("finalWord : " + finalWord);
+        console.log("stress : " + stress);
+        console.log("wordFinished : " + wordFinished);
+        console.log("---------------------");
+        if (phoneme[index] === "/") {
+
+          if (stress === -2) {
+            stress = -1;
+          }
+          wordFinished = true;
+          console.log("stress non trouvé " + stress);
+          return;
+        }
+        if (phoneme[index] === "ˈ") {
+          console.log("stress trouvé");
+          console.log("index : " + index);
+          console.log("finalWord" + finalWord);
+          if (finalWord.length > stress) {
+            stress = finalWord.length;
+          }
+        }
+
+        for (let i = 0; i < phonemedictionnary[phoneme[index]].length; i++) {
+          if (wordFinished === true) {
+            break;
+          }
+          console.log("boucle, index : " + index + ", phoneme : " + phoneme[index] + " traduction du phoeneme : " + phonemedictionnary[phoneme[index]][i]);
+          let previousfinalWord = finalWord;
+          finalWord = finalWord + phonemedictionnary[phoneme[index]][i]
+          console.log("finalWord : " + finalWord);
+
+          //if finalWord is not the same as beginning of wordToFind then don't recall the function
+          if (wordToFind.startsWith(finalWord)) {
+            console.log("Ca commence bien ");
+            findRecursiveStress(phoneme, index + 1, wordToFind, finalWord);
+          }
+          finalWord = previousfinalWord;
+
+
+        }
+
+      }
+
+      if (phoneme.includes("ˈ")) {
+        console.log("wordToFind : " + word);
+        findRecursiveStress(phoneme, index, word, finalWord);
+      }
+
+
+      console.log("-----------------------------------------------------------------------");
+      console.log("final stress : " + (stress));
+      setFinalStress((finalStress)=>stress);
+      return ;
+      //callback(); //TODO ?
+    });
+
+  }
+  
+  useEffect(() => {
+    findtheStress(chosenWord);
+  }, [chosenWord]);
+
+
+
 
   return (
     <div className="word">
       <h2>
+
         {chosenWord.split("").map((letter, index) => (
-          <Letter key={index} lettre={letter} />
+          <Letter key={index} index={index} lettre={letter} stress={finalStress} updatePoints={updatePoints}/>
+          
         ))}
       </h2>
     </div>
